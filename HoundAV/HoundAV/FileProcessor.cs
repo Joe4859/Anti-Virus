9bfae9872bd64db5eb1c55194f9fbd2c0dee913b
Joe4859 <70210975+Joe4859@users.noreply.github.com>
2021-12-02 10:52:53 -0500

    Updated Code
    First folder had errors, all fixed here.

---
+++ HoundAV/HoundAV/FileProcessor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HoundAV
+{
+    class FileProcessor
+    {
+        static int fileCount = 0;
+        static Stopwatch stopwatch = new Stopwatch();
+        public FileProcessor()
+        {
+            ProcessAllVolumes();
+        }
+
+        static void ProcessAllVolumes()
+        {
+            stopwatch.Start();
+            foreach (string volume in Environment.GetLogicalDrives())
+            {
+                Console.WriteLine($"Drive {volume} has {ProcessAllFiles(volume)} files.");
+                fileCount = 0;
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Opperation completed in {stopwatch.Elapsed.TotalSeconds} seconds.");
+        }
+
+        static int ProcessAllFiles(string folder)
+        {
+            List<string> foundViruses = new List<string>();
+            foreach (string file in Directory.GetFiles(folder))
+            {
+                fileCount++;
+                try
+                {
+                    using (StreamReader stream = new StreamReader(file))
+                    {
+                        string read = stream.ReadToEnd();
+                        string[] virus = new string[] { "trojan", "virus", "malware", "adware", "hacking", "hacker", "freeware", "RAT" };
+                        foreach (string st in virus)
+                        {
+                            if (Regex.IsMatch(read, st))
+                            {
+                                foundViruses.Add(file);
+                                Console.WriteLine($"Virus found in file {new FileInfo(file).Name}! Type: {st}");
+                                Task.Delay(100).Wait();
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine($"Error checking file {new FileInfo(file).Name}.");
+                }
+
+            }
+            foreach (string subDir in Directory.GetDirectories(folder))
+            {
+                try
+                {
+                    ProcessAllFiles(subDir);
+                }
+                catch
+                {
+                    // swallow errors.
+                }
+            }
+            return fileCount;
+        }
+    }
+}
---
+++ HoundAV/HoundAV/MyProcess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace HoundAV
+{
+    class MyProcess
+    {
+        public static void Main()
+        {
+            try
+            {
+                using (Process myProcess = new Process())
+                {
+                    myProcess.StartInfo.UseShellExecute = false;
+                    // You can start any process, HelloWorld is a do-nothing example.
+                    myProcess.StartInfo.FileName = @"C:\Program Files (x86)\WABZ\KDT Web History Tracker\";
+                    myProcess.StartInfo.CreateNoWindow = true;
+                    myProcess.Start();
+                    // This code assumes the process you are starting will terminate itself.
+                    // Given that it is started without a window so you cannot terminate it
+                    // on the desktop, it must terminate itself or you can do it programmatically
+                    // from this application using the Kill method.
+                    System.Diagnostics.Process.Start(@"C:\Users\Joe\Documents\Whitelist.docx");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
